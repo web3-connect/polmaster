9925fc10a495f037d1adf2ce7ed6fc4b57e464dc
web3-connect <dappweb3creator@gmail.com>
2024-11-11 11:24:16 +0800

    Update dapp.js

--- js/dapp.js
+++ js/dapp.js
@@ -224,7 +224,7 @@ function showRefLink (totalInvested, currentAccount) {
     if (totalInvested.eq(0)) {
         refLink.text('You will get your ref link after investing')
     } else {
-        const link = window.location.origin + '/?ref=' + currentAccount
+        const link = window.location.origin + '/polmaster/?ref=' + currentAccount
         refLink.text(link)
 
         fromEvent($('#copyButton'), 'click').pipe(takeUntil(merge(accountChangedSubject, walletChangedSubject))).subscribe(() => {
